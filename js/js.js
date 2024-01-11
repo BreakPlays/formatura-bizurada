d424eb5511dc6f6b3e33fa39aaae8bc2e08a99bb
BreakPlays <95656641+BreakPlays@users.noreply.github.com>
2024-01-11 20:50:09 -0300

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -1,28 +1,71 @@
 
+let currentAudio;
+let isLoop = false;
+let audioPath;
+
+
+document.addEventListener("orientationchange", function(event){
+    switch(window.orientation) 
+    {  
+        case -90: case 90:
+            
+            break; 
+        default:
+			alert("Mude a orientção do seu dispositivo para o modo paisagem")
+    }
+});
 
 
 $('th').on('click', function(){
-	let folderName = $(this).parent().attr('id')
+	let folderName = $(this).parent().attr('class')
 	
 	if($(this).text() == ""){
 		return
 	}
 	
 	
-	let command = $(this).text().toLowerCase().replace(' ','')
+	let command = $(this).text().toLowerCase().replaceAll(' ','')
 	
 	
 	
 	if (command == "ordinariomarche"){
 		folderName = "mixed"
 	}
+	
+	if (command.indexOf("loop") > 0){
+		isLoop = 1
+	}
 	$('#audioPlayingName').text(`"${$(this).text()}"`)
 	
+	
+	
+	
 	let toque = new Audio(`sounds/${folderName}/${command}.wav`)
-	toque.play()
+	currentAudio = toque
+	if (isLoop){
+		audioPath = `sounds/${folderName}/${command}.wav`
+		
+		loopify(audioPath, function(err,loop) {
+
+		  if (err) {
+			return console.err(err);
+		  }
+		  loop.play();
+		})
+
+	}else{
+		currentAudio.play()	
+	}
+	
+	
 	
 	$('#currentAudioPlaying').css('opacity','1')
 	$('#currentAudioPlaying').css('pointer-events','all')
+	
+	if(isLoop){
+		loopAudio()
+		return
+	}
 	
 	toque.onended = function(){
 		$('#currentAudioPlaying').css('opacity','0')
@@ -31,3 +74,38 @@ $('th').on('click', function(){
 	
 
 })
+
+
+
+document.addEventListener('keydown', evt => {
+	
+	if (currentAudio != undefined || currentAudio != null) {
+	}else {
+		return
+	}
+	if (evt.key === 'Escape') {
+		if(isLoop){
+			currentAudio.onended = function(){
+				pressEsq()
+			}
+			return
+		}
+     	pressEsq()
+    }
+})
+
+
+function loopAudio(){
+	currentAudio.onended = function(){
+		currentAudio.play()
+	}
+}
+
+function pressEsq(){
+	console.log("esq")
+	currentAudio.pause()
+	currentAudio.currentTime = 0
+	currentAudio = null
+	$('#currentAudioPlaying').css('opacity','0')
+	$('#currentAudioPlaying').css('pointer-events','none')
+}
